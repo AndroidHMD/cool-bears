f8b934374f7c2c180e6b304d133d71b852af2cd2
codingInSpace <jonathan.grangien@gmail.com>
2016-04-16 14:30:58 +0200

    Implement basic move script on teddy bear

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -1,24 +1,31 @@
-/*using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyControl : MonoBehaviour {
-    float speed;
+    float moveSpeed;
+    //public GameObject player;
+    public Transform target;
 
     void Start () {
- 	    speed = 2.0f;
+ 	     moveSpeed = 0.1f;
+ 	     //player = GameObject.Find("Main Camera");
 	}
 
 	void Update () {
-	    Vector3 position = new transform.position;
+	    //Vector3 position = new transform.position;
+	    //position = new Vector3(position.x, position.y - speed * Time.deltaTime);
+	    //target.position = player.transform.position;
 
-	    position = new Vector3(position.x, position.y - speed * Time.deltaTime);
+	    transform.LookAt(target.position);
+	    float step = moveSpeed * Time.deltaTime;
+	    transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	    //transform.position = new Vector3(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime);
 
-	    transform.position = position;
+	    //transform.position = position;
+	    //Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
 
-	    Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-
-	    if (transform.position.y < min.y) {
-	    	Destroy(gameObject);
-	    }
+	    //if (transform.position.y < min.y) {
+	    //	Destroy(gameObject);
+	    //}
 	}
-}*/
+}
